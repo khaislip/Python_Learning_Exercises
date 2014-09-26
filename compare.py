f422f28c6d36473f6c544e4611e6b6f8f5397d4d
khaislip <khaislip@gmail.com>
2014-09-26 01:29:39 -0400

    Docs
    Doc cleanup

--- compare.py
+++ compare.py
@@ -4,17 +4,17 @@ import os
 
 with open('neo3_ecs_rules_only_latest.txt') as inputFile1, open('normalized_inputFile1.txt', 'w') as outputFile1:
  for line in inputFile1:
-   outputFile1.write(line.strip(' \t\n\r')) #get rid of all whitespace
+   outputFile1.write(line.strip(' \t\n\r')) #get rid of whitespace
    outputFile1.write('\n')
 
 with open ('normalized_inputFile1_final.txt', 'w') as outputFile1_final:
  outputFile1_final.write(os.linesep.join([l for l in open("normalized_inputFile1.txt","r").read().splitlines() if l])) #get rid of blank lines 
 
-#normalizes file2 of whitespace
+#normalizes inputFile2 of whitespace
 
 with open('Neo_1412_ECS_Rule_Final.txt') as inputFile2, open('normalized_inputFile2.txt', 'w') as outputFile2:
  for line in inputFile2:
-  outputFile2.write(line.strip(' \t\n\r')) #get rid of all whitespace
+  outputFile2.write(line.strip(' \t\n\r')) #get rid of whitespace
   outputFile2.write('\n')
 
 with open ('normalized_inputFile2_final.txt', 'w') as outputFile2_final:
